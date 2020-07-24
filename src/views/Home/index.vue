6726d16eeadac7a6ddc8c9a190eb62a3e91ab2d3
NaweedSufranj <sufranj.naweed@gmail.com>
2020-07-24 07:04:28 +0200

    add : base navigation

---
+++ src/views/Home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+      <h1>Kraken Weather</h1>
+      <nav>
+          <ul>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name : "Home",
+}
+</script>
+
+<style>
+header{
+    display: flex;
+    width: 100%;
+    align-items: center;
+}
+
+h1{
+    width: 60%;
+}
+nav{
+    width: 40%;
+}
+ul{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+}
+a{
+    display: block;
+}
+</style>
