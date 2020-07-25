c5ee7941f0b007f387e517f25df2a5bfec937c71
SufranjNaweed <sufranj.naweed@gmail.com>
2020-07-26 01:27:19 +0200

    add : formsearchweather component

---
+++ src/components/Weather/FormSearchWeather/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit="onSubmit">
+        <input type="text" name="city" id="city" v-model="city">
+        <input type="submit" value="SEARCH">
+    </form>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+
+export default {
+    name : "FormSearchWeather",
+    data : function() {
+        return {
+            city : ""
+        }
+    },
+    methods : {
+        ...mapActions(['searchCity']),
+        onSubmit(e){
+            e.preventDefault();
+            this.searchCity(this.city);
+            this.city = "";
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
