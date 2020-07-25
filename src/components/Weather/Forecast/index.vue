80a66eda4794a3a8905e135245e5269cd47ad11f
SufranjNaweed <sufranj.naweed@gmail.com>
2020-07-26 01:25:09 +0200

    add : Forecast & Today component

---
+++ src/components/Weather/Forecast/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="forecast">
+        <h2>Forecast</h2>
+        <div :key="forecast.id" v-for="forecast in forecasts"> 
+            <div class="card-forecast">
+                <p>{{forecast.date}}</p>
+                <div class="forecast-temperature">
+                    <img :src=forecast.day.condition.icon alt="forecastIcon">
+                    <p class="forecast-temperature-state">{{forecast.day.condition.text}}</p>
+                    <div class="min-max-container">
+                        <div class="min-forecast">
+                            <span>MIN </span>
+                            <span>{{forecast.day.mintemp_c}} C</span>
+                        </div>
+                        <div class="max-forecast">
+                            <span>MAX </span>
+                            <span>{{forecast.day.maxtemp_c}} C</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="forecast-general-info">
+                    <p class="forecast-info">WIND SPEED : {{forecast.day.maxwind_kph}} KM/H</p>
+                    <p class="forecast-info">HUMUDITY : {{forecast.day.avghumidity}} %</p>
+                    <p class="forecast-info">CHANCE OF RAIN : {{forecast.day.daily_chance_of_rain}} %</p>
+                    <p class="forecast-info">SUNRISE : {{forecast.astro.sunrise}}</p>
+                    <p class="forecast-info">SUNSET  : {{forecast.astro.sunset}}</p>
+                    <p class="forecast-info">MOONSET : {{forecast.astro.moonrise}}</p>
+                    <p class="forecast-info">MOONSET : {{forecast.astro.moonset}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "Forecast",
+    props : {
+        forecasts : Array
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Weather/Today/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="today">
+        <h2>Today</h2>
+        <div class="container-today" v-if="today.current">
+            <div class="left">{{today.current.temp_c}} C</div>
+            <div class="middle">
+                <p class="city">{{today.location.name}}</p>
+                <div class="today-icon"><img :src=today.current.condition.icon alt=""></div>
+                <p class="today-type">{{today.current.condition.text}}</p>
+            </div>
+            <div class="right">
+                <p class="today-info">UV : {{today.current.uv}}</p>
+                <p class="today-info">wind direction : {{today.current.wind_dir}}</p>
+                <p class="today-info">wind speed : {{today.current.wind_kph}} km/h</p>
+                <p class="today-info">humidity : {{today.current.humidity}} %</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "Today",
+    props : {
+        today : Object
+    },
+    created() {
+        console.log("today => ", this.today)  //undefined;
+    },
+}
+</script>
+
+<style>
+
+</style>
