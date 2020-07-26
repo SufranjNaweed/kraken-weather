7a7c7230c1264b76893ac88360f8b90ef1be925a
SufranjNaweed <sufranj.naweed@gmail.com>
2020-07-26 22:54:58 +0200

    add : dist/

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/kraken-weather/'
-      : '/kraken-weather/'
+      ? '/kraken-weather/dist/'
+      : '/kraken-weather/dist/'
   }
