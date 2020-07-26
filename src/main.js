34e0d7b2f9d19b77632de4fe03bb5ee96128bb27
SufranjNaweed <sufranj.naweed@gmail.com>
2020-07-26 05:36:51 +0200

    init : bulma & moment

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import moment from 'moment'
+
+Vue.use(Buefy)
+Vue.prototype.moment = moment
 Vue.config.productionTip = false
 
 new Vue({
